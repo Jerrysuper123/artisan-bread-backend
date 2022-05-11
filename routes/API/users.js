70f02639645c624a7b7284fdd1aad5ced787f59f
Jerry Chen <jerryq.chen@gmail.com>
2022-05-11 11:54:39 +0800

    added user registration validation

--- routes/API/users.js
+++ routes/API/users.js
@@ -30,18 +30,61 @@ const { User } = require("../../models");
 router.post("/register", async (req, res) => {
   // console.log(req.body);
   try {
-    const user = new User({
-      username: req.body.username,
-      password: getHashedPassword(req.body.password),
-      email: req.body.email,
-    });
-    await user.save();
-    res.sendStatus(200);
+    let usernameInput = req.body.username;
+    let passwordInput = req.body.password;
+    let emailInput = req.body.email;
+    console.log(req.body);
+
+    if (validationRegistration(usernameInput, emailInput, passwordInput)) {
+      const user = new User({
+        username: usernameInput,
+        password: getHashedPassword(passwordInput),
+        email: emailInput,
+      });
+      await user.save();
+      res.sendStatus(200);
+    }
   } catch (e) {
     console.log(e);
     res.sendStatus(422);
   }
 });
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+//validate registration
+const validationRegistration = (usernameInput, emailInput, passwordInput) => {
+  let noValidateError = true;
+  // console.log("username", typeof username);
+
+  if (usernameInput === "") {
+    console.log("empty string");
+    noValidateError = false;
+  }
+
+  if (emailInput === "" || !validateEmail(emailInput)) {
+    console.log("invalid email");
+    noValidateError = false;
+  }
+
+  let passw = /^[A-Za-z]\w{7,14}$/;
+  if (passwordInput === "" || !String(passwordInput).match(passw)) {
+    console.log("invalid password");
+    noValidateError = false;
+  }
+
+  if (noValidateError) {
+    console.log("validated success");
+  }
+
+  return noValidateError;
+};
 
 //base url/api/users/login
 router.post("/login", async (req, res) => {
